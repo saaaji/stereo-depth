25e7cd8a92e8ed103bd38ac67c7aaf7bcc0e32b9
siraaj <saaaji.sss@gmail.com>
2025-04-15 20:40:53 -0600

    old changes

--- src/cameracal.py
+++ src/cameracal.py
@@ -34,10 +34,6 @@ def main():
     checkerboard[:, :, 0:2] = np.mgrid[0:args.width, 0:args.len].T * args.quadSize
     checkerboard = checkerboard.reshape((args.width * args.len, 3))
 
-    # create calibration arrays
-    objectPoints = []
-    imagePoints = [[], []]
-
     if args.freq > 0:
         print('using continuous capture')
 
@@ -54,6 +50,11 @@ def main():
         exit()
 
     # assume resolution is the same for both
+    left.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+    left.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+    right.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+    right.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+
     cap_width = int(left.get(cv2.CAP_PROP_FRAME_WIDTH))
     cap_height = int(left.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
@@ -66,7 +67,71 @@ def main():
         exit()
 
     endTime = time.time() + float(match.group(1)) * 60 + float(match.group(2))
-    while time.time() < endTime:
+    scale = 0.8
+    calib = []
+
+    for (cam, name) in zip([left, right], ['left', 'right']):
+        imagePoints = []
+        objectPoints = []
+        nCaps = 0
+        while True:
+            (ret, image) = cam.read()
+            if not ret:
+                break
+            gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+            # gray = cv2.resize(image, None, fx=scale, fy=scale, interpolation=cv2.INTER_LINEAR)
+            # gray = cv2.cvtColor(gray, cv2.COLOR_BGR2GRAY)
+
+            (ret, corners) = cv2.findChessboardCorners(gray, (args.width, args.len), flags=cv2.CALIB_CB_ADAPTIVE_THRESH + cv2.CALIB_CB_NORMALIZE_IMAGE + cv2.CALIB_CB_EXHAUSTIVE)
+
+            # corners /= scale
+            vis = gray
+            if ret:
+                corners = cv2.cornerSubPix(gray, corners, (5, 5), (-1, -1), (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001))
+                vis = cv2.drawChessboardCorners(gray, (args.width, args.len), corners, ret)
+
+            vis = cv2.resize(vis, None, fx=0.3, fy=0.3, interpolation=cv2.INTER_LINEAR)
+
+            key = cv2.waitKey(1)
+            if key == ord('p') and ret:
+                nCaps += 1
+                print(f'capturing... {nCaps}')            
+                objectPoints.append(checkerboard)
+                imagePoints.append(corners)
+            elif key == ord('q'):
+                break
+            
+            cv2.imshow('capture', vis)
+
+        
+        if len(objectPoints) > 0:
+            print(f'computing intrinsics with {len(objectPoints)} frames...')
+            ret, mat, dist, rvecs, tvecs = cv2.calibrateCamera(objectPoints, imagePoints, (cap_width, cap_height), None, None)
+
+            output = {
+                'mat': mat.tolist(),
+                'dist': dist.tolist(),
+                # 'rvecs': rvecs.tolist(),
+                # 'tvecs': tvecs.tolist(),
+            }
+
+            calib.append({
+                'mat': mat,
+                'dist': dist,
+            })
+
+            with open(f'{name}.yaml', 'w+') as outfile:
+                yaml.dump(output, outfile)
+                print(f'output intrinsics written to \'{name}.yaml\' (ERROR: {ret})')
+        else:
+            print(f'no checkerboard found for {name}')
+
+
+    # create calibration arrays
+    objectPoints = []
+    imagePoints = [[], []]
+    nCaps = 0
+    while True:
         reads = (left.read(), right.read())
         success = [r[0] for r in reads]
         frames = [r[1] for r in reads]
@@ -78,45 +143,62 @@ def main():
             print('cannot receive frame. exiting...')
             break
         
-        scale = 1/4
         graysFull = [cv2.cvtColor(f, cv2.COLOR_BGR2GRAY) for f in frames]
-        grays = [
-            cv2.resize(f, None, fx=scale, fy=scale, interpolation=cv2.INTER_LINEAR)
-            for f in graysFull
-        ]
+        # grays = [
+        #     cv2.resize(f, None, fx=scale, fy=scale, interpolation=cv2.INTER_LINEAR)
+        #     for f in frames
+        # ]
+
+        grays = [f for f in frames]
+        grays = [cv2.cvtColor(g, cv2.COLOR_BGR2GRAY) for g in grays]
+        # graysFull = [cv2.inRange(g, 0, 100) for g in graysFull]
+        # kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (10, 10))
+        # graysFull = [cv2.dilate(g, kernel, iterations=1) for g in graysFull]
+        # graysFull = [cv2.medianBlur(g, 1) for g in graysFull]
+        # min = np.array([0, 0, 0])
+        # max = np.array([179, 50, 255])
+        # # masked = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+        # masks = [cv2.inRange(g, min, max) for g in graysFull]
+        # kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (50, 30))
+        # graysFull = [cv2.dilate(m, kernel, iterations=5) for m in masks]
+        # graysFull = [255 - cv2.bitwise_and(g, m) for g, m in zip(graysFull, masks)]
         
         searches = [
-            cv2.findChessboardCorners(f, (args.width, args.len), None)
-            for f in grays
+            cv2.findChessboardCorners(g, (args.width, args.len), flags=cv2.CALIB_CB_ADAPTIVE_THRESH + cv2.CALIB_CB_NORMALIZE_IMAGE + cv2.CALIB_CB_EXHAUSTIVE)
+            for g in grays
         ]
         
         success = [s[0] for s in searches]
         corners = [s[1] for s in searches]
         
         # if both cameras saw chessboard
+        # vis = [graysFull[0], cv2.cvtColor(grays[0], cv2.COLOR_BGR2GRAY)]
+        # if all(success):
         vis = graysFull
+        
+        key = cv2.waitKey(1)
+
         if all(success):
-            corners = [c / scale for c in corners]
+            # corners = [c / scale for c in corners]
             corners = [
                 cv2.cornerSubPix(g, c, (5, 5), (-1, -1), (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001))
-                for g, c in zip(graysFull, corners)
-            ]
-            
+                for g, c in zip(graysFull, corners)]
+            vis = [
+                cv2.drawChessboardCorners(f, (args.width, args.len), c, s)
+                for f, c, s in zip(graysFull, corners, success)]
+
+        if key == ord('p') and all(success):
+            nCaps += 1
+            print(f'capturing... {nCaps}')            
             objectPoints.append(checkerboard)
             for i, c in enumerate(corners):
                 imagePoints[i].append(c)
-            
-            vis = [
-                cv2.drawChessboardCorners(g, (args.width, args.len), c, s)
-                for g, c, s in zip(graysFull, corners, success)
-            ]
-        
-        vis = np.concatenate(vis, axis=1)
-        vis = cv2.resize(vis, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_LINEAR)
-        cv2.imshow('capture', vis)
-        
-        if cv2.waitKey(1) == ord('q'):
+        elif key == ord('q'):
             break
+
+        vis = np.concatenate(vis, axis=1)
+        vis = cv2.resize(vis, None, fx=0.25, fy=0.25, interpolation=cv2.INTER_LINEAR)
+        cv2.imshow('capture', vis)
 
     if args.freq > 0:
         objectPoints = [x for (i, x) in enumerate(objectPoints) if not i % args.freq]
@@ -130,12 +212,16 @@ def main():
             objectPoints, 
             imagePoints[0], 
             imagePoints[1],
-            None,
-            None,
-            None,
-            None,
+            calib[0].get('mat'),
+            calib[0].get('dist'),
+            calib[1].get('mat'),
+            calib[1].get('dist'),
             (cap_width, cap_height),
-            flags=0)
+            criteria=(cv2.TERM_CRITERIA_EPS +
+                         cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001),
+            flags=0,
+            R=None,
+            T=None)
 
         output = {
             'leftMat': cm1.tolist(),
@@ -148,37 +234,10 @@ def main():
 
         with open(OUT_FNAME, 'w+') as outfile:
             yaml.dump(output, outfile)
-            print(f'output intrinsics written to \'{OUT_FNAME}\'')
+            print(f'output intrinsics written to \'{OUT_FNAME}\' (ERROR: {ret})')
     else:
         print('no checkerboard found. exiting...')
         exit()
-
-    while True:
-        reads = (left.read(), right.read())
-        success = [r[0] for r in reads]
-        frames = [r[1] for r in reads]
-
-        if args.flipIndex:
-            frames[args.flipIndex] = cv2.rotate(frames[args.flipIndex], cv2.ROTATE_180)
-        
-        
-        dist = np.concatenate(frames, axis=1)
-        dist = cv2.resize(dist, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_LINEAR)
-        # r1, r2, p1, p2 = cv2.stereoRectify(cm1, dist1, cm2, dist2, (cap_width, cap_height), R, T)
-
-        
-        undist = [
-            cv2.undistort(f, c, d)
-            for f, c, d in zip(frames, [cm1, cm2], [dist1, dist2])
-        ]
-        undist = np.concatenate(undist, axis=1)
-        undist = cv2.resize(undist, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_LINEAR)
-
-        grid = np.concatenate((dist, undist), axis=0)
-        cv2.imshow('capture', grid)
-
-        if cv2.waitKey(1) == ord('q'):
-            break
     
     left.release()
     right.release()
--- src/undistort.py
+++ src/undistort.py
@@ -8,107 +8,252 @@ import usb.util
 import sys
 import time
 
-IN_FNAME = 'intrinsics_out.yaml'
+IN_FNAME = './intrinsics_out.yaml'
 
 def main():
     parser = argparse.ArgumentParser(
         prog='Undistort',
         description='Check camera intrinsics.')
 
-    parser.add_argument('src', type=str, help='filename of video source (/dev/...)')
+    parser.add_argument('srcl', type=str, help='filename of video source for left (/dev/...)')
+    parser.add_argument('srcr', type=str, help='filename of video source for right (/dev/...)')
+    parser.add_argument('-i', '--index', type=int, required=False, dest='flipIndex', help='index of camera to flip, if necessary (0=left, 1=right)')
+
     args = parser.parse_args()
 
-    cap = cv2.VideoCapture(args.src, cv2.CAP_V4L2)
-    cap.set(cv2.CAP_PROP_MODE, 1)
+    left = cv2.VideoCapture(args.srcl, cv2.CAP_V4L2)
+    right = cv2.VideoCapture(args.srcr, cv2.CAP_V4L2)
+    fail = False
 
-    '''
-    dev=usb.core.find(idVendor=0x2560, idProduct=0xc128) #lsusb to list attached devices in case your id's are different. 
-    #print(dev) #uncomment to see the configuration tree. 
-    #Follow the tree: dev[0] = configuration 1. 
-    #interfaces()[2] = HID interface
-    #0x06 = Interrupt OUT. (Endpoint)
-
-    if dev is None:
-        raise ValueError('Device not found')
-    cfg=-1
-
-    i = dev[0].interfaces()[2].bInterfaceNumber
-
-    cfg = dev.get_active_configuration()
-    intf = cfg[(2,0)]
-
-    if dev.is_kernel_driver_active(i):
-        try:
-            reattach = True
-            dev.detach_kernel_driver(i)
-            #print("eh") #debug making sure it got in here. 
-        except usb.core.USBError as e:
-            sys.exit("Could not detach kernel driver from interface({0}): {1}".format(i, str(e)))
-
-
-    print(dev) #not needed, just helpful for debug
-    msg = [0] * 64
-    #msg = [0xA0, 0xc1, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00] #gotta send a 64 byte message. 
-    msg[0] = 0xA8 #these command sets are in the qtcam source code. 
-    msg[1] = 0x1c
-    msg[2] = 0x01 # 01= ext trigger. 00 = Master mode. 
-    msg[3] = 0x00
-
-
-    dev.write(0x6,msg,1000) #wham bam, than
-    time.sleep(1)
-    # dev.attach_kernel_driver(i)
-
-    # cap.set(cv2.CAP_PROP_FOURCC,cv2.VideoWriter_fourcc(*'UYVY'));
-    # cap.set(cv2.CAP_PROP_FORMAT, -1);
-
-    # if (!cap.open(0,cv2.CAP_V4L)) {
-    #     printf("ERROR! Unable to open camera\n");
-    # }
-    # cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920);
-    # cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080);
-    # # cap.set(cv2.CAP_PROP_FPS, 60);
-    cap.set(cv2.CAP_PROP_AUTO_EXPOSURE, 0.25)
-    cap.set(cv2.CAP_PROP_EXPOSURE, 0.03)
-    cap.set(cv2.CAP_PROP_AUTO_WB, 1)
-    # cap.set(cv2.CAP_PROP_WB_TEMPERATURE, 6000)
-    '''
-    
-    if not cap.isOpened():
-        print(f'cannot open camera \'{args.src}\'. exiting...')
+    if not left.isOpened():
+        print(f'cannot open camera \'{args.srcl}\'. exiting...')
+        fail = True
+    if not right.isOpened():
+        print(f'cannot open camera \'{args.srcr}\'. exiting...')
+        fail = True
+    if fail:
         exit()
 
-    cap_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-    cap_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    # fix dimensions
+    left.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+    left.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+    right.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+    right.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
 
-    print(f'using camera \'{args.src}\' ({cap_width}x{cap_height})')
+    cap_width = int(left.get(cv2.CAP_PROP_FRAME_WIDTH))
+    cap_height = int(left.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+    # check dimensions
+    cap_width_r = int(right.get(cv2.CAP_PROP_FRAME_WIDTH))
+    cap_height_r = int(right.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    print(f'RDIM: {cap_width_r} {cap_height_r}')
+
+    print(f'using cameras \'{args.srcl}\' and \'{args.srcr}\' ({cap_width}x{cap_height})')
+
+    stereo = cv2.StereoBM_create()
+    # stereo.setMinDisparity(4)
+    stereo.setNumDisparities(16)
+    stereo.setBlockSize(15)
+    # stereo.setSpeckleRange(16)
+    # stereo.setSpeckleWindowSize(45)
+
+    def nothing(x):
+        pass
+    
+    cv2.namedWindow('disp', cv2.WINDOW_NORMAL)
+    cv2.resizeWindow('disp', 600, 600)
+    
+    cv2.createTrackbar('numDisparities','disp',1,200,nothing)
+    cv2.createTrackbar('blockSize','disp',5,200,nothing)
+    cv2.createTrackbar('preFilterType','disp',1,1,nothing)
+    cv2.createTrackbar('preFilterSize','disp',2,100,nothing)
+    cv2.createTrackbar('preFilterCap','disp',5,100,nothing)
+    cv2.createTrackbar('textureThreshold','disp',10,100,nothing)
+    cv2.createTrackbar('uniquenessRatio','disp',15,100,nothing)
+    cv2.createTrackbar('speckleRange','disp',0,100,nothing)
+    cv2.createTrackbar('speckleWindowSize','disp',3,25,nothing)
+    cv2.createTrackbar('disp12MaxDiff','disp',5,25,nothing)
+    cv2.createTrackbar('minDisparity','disp',5,25,nothing)
 
     with open(IN_FNAME) as infile:
         intrinsics_dict = yaml.load(infile, Loader=yaml.FullLoader)
-        print(intrinsics_dict)
-        if 'cameraMat' in intrinsics_dict and 'distCoeffs' in intrinsics_dict:
-            cameraMat = np.array(intrinsics_dict['cameraMat'], dtype=np.float32)
-            distCoeffs = np.array(intrinsics_dict['distCoeffs'], dtype=np.float32)
+        props = ['leftDistCoeffs', 'leftMat', 'rightDistCoeffs', 'rightMat', 'rotMat', 'tranMat']
+        
+        if all(prop in intrinsics_dict for prop in props):
+            leftDistCoeffs = np.array(intrinsics_dict['leftDistCoeffs'], dtype=np.float32)
+            leftMat = np.array(intrinsics_dict['leftMat'], dtype=np.float32)
+            rightDistCoeffs = np.array(intrinsics_dict['rightDistCoeffs'], dtype=np.float32)
+            rightMat = np.array(intrinsics_dict['rightMat'], dtype=np.float32)
+            rotMat = np.array(intrinsics_dict['rotMat'], dtype=np.float64)
+            tranMat = np.array(intrinsics_dict['tranMat'], dtype=np.float64)
 
-            print(cameraMat)
-            print(distCoeffs)
+
+
+            R1, R2, P1, P2, Q, roi1, roi2 = cv2.stereoRectify(
+                leftMat, leftDistCoeffs, 
+                rightMat, rightDistCoeffs, 
+                (cap_width, cap_height), 
+                rotMat,
+                tranMat,
+                alpha=0)
+            # print(roi1)
+            # print(leftMat)
+            # print(P1)
+            # print(cv2.getDefaultNewCameraMatrix(leftMat))
+            # print(rightMat)
+            # print(P2)
+
+            [leftMap1, leftMap2] = cv2.initUndistortRectifyMap(
+                cameraMatrix=leftMat, 
+                distCoeffs=leftDistCoeffs, 
+                R=R1, 
+                newCameraMatrix=P1,
+                size=(cap_width, cap_height), 
+                m1type=cv2.CV_16SC2)
+            # [leftMap1, leftMap2] = cv2.initUndistortRectifyMap(
+            #     cameraMatrix=leftMat, 
+            #     distCoeffs=leftDistCoeffs, 
+            #     R=np.identity(3),
+            #     newCameraMatrix=cv2.getDefaultNewCameraMatrix(leftMat),
+            #     size=(cap_width, cap_height), 
+            #     m1type=cv2.CV_16SC2)
+            [rightMap1, rightMap2] = cv2.initUndistortRectifyMap(rightMat, rightDistCoeffs, R2, P2, (cap_width, cap_height), m1type=cv2.CV_16SC2)
             
             while True:
-                ret, frame = cap.read()
-                
-                if not ret:
-                    print('cannot receive frame. exiting...')
-                    break
+                reads = (left.read(), right.read())
+                success = [r[0] for r in reads]
+                frames = [r[1] for r in reads]
 
-                unFrame = cv2.undistort(frame, cameraMat, distCoeffs)
-                compFrame = np.concatenate((frame, unFrame), axis=0)
-                compFrame = cv2.resize(compFrame, None, fx=1/2, fy=1/2, interpolation=cv2.INTER_LINEAR)
-                cv2.imshow('capture', compFrame)
+                # frames = list(reversed(frames))
+
+                if args.flipIndex is not None:
+                    frames[args.flipIndex] = cv2.rotate(frames[args.flipIndex], cv2.ROTATE_180)
+                
+                if not all(success):
+                    print('cannot receive frame. exiting...')
+        
+                scale = 0.25
+
+                dist = np.concatenate(frames, axis=1)
+                dist = cv2.resize(dist, None, fx=scale, fy=scale, interpolation=cv2.INTER_LINEAR)
+
+                undist1 = [
+                    cv2.undistort(f, c, d)
+                    for f, c, d in zip(frames, [leftMat, rightMat], [leftDistCoeffs, rightDistCoeffs])
+                ]
+                undist1 = np.concatenate(undist1, axis=1)
+                undist1 = cv2.resize(undist1, None, fx=scale, fy=scale, interpolation=cv2.INTER_LINEAR)
+
+                undist2 = [
+                    f
+                    for f in frames
+                ]
+                
+                undist2 = [
+                    cv2.remap(f, m1, m2, interpolation=cv2.INTER_LINEAR)
+                    for f, m1, m2 in zip(frames, [leftMap1, rightMap1], [leftMap2, rightMap2])
+                ]
+                undist2 = [
+                    cv2.cvtColor(f, cv2.COLOR_BGR2GRAY)
+                    for f in undist2
+                ]
+                # gray = [cv2.cvtColor(f, cv2.COLOR_BGR2GRAY) for f in undist2]
+                disp = stereo.compute(undist2[0], undist2[1])
+                disp = disp / disp.max() * 3
+                disp = cv2.resize(disp, None, fx=1/2, fy=1/2, interpolation=cv2.INTER_LINEAR)
+                # disp = np.concatenate(undist2, axis=1)
+
+                undist2 = np.concatenate(undist2, axis=1)
+                undist2 = cv2.resize(undist2, None, fx=scale, fy=scale, interpolation=cv2.INTER_LINEAR)
+                grid = np.concatenate((dist, undist1), axis=0)
+
+                numDisparities = cv2.getTrackbarPos('numDisparities','disp')*16
+                blockSize = cv2.getTrackbarPos('blockSize','disp')*2 + 5
+                preFilterType = cv2.getTrackbarPos('preFilterType','disp')
+                preFilterSize = cv2.getTrackbarPos('preFilterSize','disp')*2 + 5
+                preFilterCap = cv2.getTrackbarPos('preFilterCap','disp')
+                textureThreshold = cv2.getTrackbarPos('textureThreshold','disp')
+                uniquenessRatio = cv2.getTrackbarPos('uniquenessRatio','disp')
+                speckleRange = cv2.getTrackbarPos('speckleRange','disp')
+                speckleWindowSize = cv2.getTrackbarPos('speckleWindowSize','disp')*2
+                disp12MaxDiff = cv2.getTrackbarPos('disp12MaxDiff','disp')
+                minDisparity = cv2.getTrackbarPos('minDisparity','disp')
+                
+                # Setting the updated parameters before computing disparity map
+                stereo.setNumDisparities(numDisparities)
+                stereo.setBlockSize(blockSize)
+                stereo.setPreFilterType(preFilterType)
+                stereo.setPreFilterSize(preFilterSize)
+                stereo.setPreFilterCap(preFilterCap)
+                stereo.setTextureThreshold(textureThreshold)
+                stereo.setUniquenessRatio(uniquenessRatio)
+                stereo.setSpeckleRange(speckleRange)
+                stereo.setSpeckleWindowSize(speckleWindowSize)
+                stereo.setDisp12MaxDiff(disp12MaxDiff)
+                stereo.setMinDisparity(minDisparity)
+
+                cv2.imshow('capture', grid)
+                cv2.imshow('undist', undist2)
+                cv2.imshow('disparity', disp)
 
                 if cv2.waitKey(1) == ord('q'):
                     break
+            
         else:
             print(f'\'{IN_FNAME}\' has not been populated. exiting...')
 
 if __name__ == '__main__':
     main()
+
+'''
+dev=usb.core.find(idVendor=0x2560, idProduct=0xc128) #lsusb to list attached devices in case your id's are different. 
+#print(dev) #uncomment to see the configuration tree. 
+#Follow the tree: dev[0] = configuration 1. 
+#interfaces()[2] = HID interface
+#0x06 = Interrupt OUT. (Endpoint)
+
+if dev is None:
+    raise ValueError('Device not found')
+cfg=-1
+
+i = dev[0].interfaces()[2].bInterfaceNumber
+
+cfg = dev.get_active_configuration()
+intf = cfg[(2,0)]
+
+if dev.is_kernel_driver_active(i):
+    try:
+        reattach = True
+        dev.detach_kernel_driver(i)
+        #print("eh") #debug making sure it got in here. 
+    except usb.core.USBError as e:
+        sys.exit("Could not detach kernel driver from interface({0}): {1}".format(i, str(e)))
+
+
+print(dev) #not needed, just helpful for debug
+msg = [0] * 64
+#msg = [0xA0, 0xc1, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00] #gotta send a 64 byte message. 
+msg[0] = 0xA8 #these command sets are in the qtcam source code. 
+msg[1] = 0x1c
+msg[2] = 0x01 # 01= ext trigger. 00 = Master mode. 
+msg[3] = 0x00
+
+
+dev.write(0x6,msg,1000) #wham bam, than
+time.sleep(1)
+# dev.attach_kernel_driver(i)
+
+# cap.set(cv2.CAP_PROP_FOURCC,cv2.VideoWriter_fourcc(*'UYVY'));
+# cap.set(cv2.CAP_PROP_FORMAT, -1);
+
+# if (!cap.open(0,cv2.CAP_V4L)) {
+#     printf("ERROR! Unable to open camera\n");
+# }
+# cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920);
+# cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080);
+# # cap.set(cv2.CAP_PROP_FPS, 60);
+cap.set(cv2.CAP_PROP_AUTO_EXPOSURE, 0.25)
+cap.set(cv2.CAP_PROP_EXPOSURE, 0.03)
+cap.set(cv2.CAP_PROP_AUTO_WB, 1)
+# cap.set(cv2.CAP_PROP_WB_TEMPERATURE, 6000)
+'''
